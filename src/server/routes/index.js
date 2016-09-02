ef00f3f189edb61009b1316dac78c27d3f28ecc2
johnmberger <johnmartinberger@gmail.com>
2016-09-02 14:20:47 -0600

    added add contact functionality

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,11 +6,44 @@ const indexController = require('../controllers/index');
 
 router.get('/contacts', function (req, res, next) {
 
+  if (req.query.first_name) {
+    const query = req.query.first_name;
+    const renderObject = {
+      title:'Contacts:'
+    };
+    db.any(`SELECT * FROM contacts WHERE lower(first_name) LIKE '%${query}%'`, [true])
+    .then((data) => {
+      renderObject.data = data;
+      res.render('contacts', renderObject);
+    })
+    .catch((error) => {
+      next(error);
+    });
+  } else {
+    const renderObject = {
+      title:'Contacts'
+    };
+
+    db.any(`SELECT * FROM contacts`, [true])
+    .then((data) => {
+      renderObject.data = data;
+      res.render('contacts', renderObject);
+    })
+    .catch((error) => {
+      next(error);
+    });
+  }
+});
+
+router.get('/contacts/:id', function (req, res, next) {
+
   const renderObject = {
-    title:'Listing All People'
+    title:'Contacts'
   };
 
-  db.any(`SELECT * FROM contacts`, [true])
+  const contactID = req.params.id;
+
+  db.any(`SELECT * FROM contacts WHERE id = ${contactID}`, [true])
   .then((data) => {
     renderObject.data = data;
     res.render('contacts', renderObject);
@@ -20,18 +53,11 @@ router.get('/contacts', function (req, res, next) {
   });
 });
 
-router.get('/contacts/:id', function (req, res, next) {
+router.post('/contacts', (req, res, next) => {
 
-  const renderObject = {
-    title:'Listing One Person'
-  };
-
-  const contactID = req.params.id;
-
-  db.any(`SELECT * FROM contacts WHERE id = ${contactID}`, [true])
+  db.any(`INSERT INTO contacts (first_name, last_name, dob, eye_color, telephone_number) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.dob}', '${req.body.eye_color}', '${req.body.telephone_number}')`, [true])
   .then((data) => {
-    renderObject.data = data;
-    res.render('contacts', renderObject);
+    res.redirect('http://localhost:3000/contacts');
   })
   .catch((error) => {
     next(error);
